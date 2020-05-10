e34f67cd202962e06db277bcff35c8bef2e5753d
Dhayana <contatodhayana@gmail.com>
2020-05-10 18:48:05 -0300

    cabeçalho

---
+++ script.js
@@ -0,0 +1,10 @@
+const bt_navegacao = document.getElementById("bt-navegacao");
+const navegacao = document.getElementById("navegacao");
+
+bt_navegacao.addEventListener("click", () => {
+    navegacao.classList.toggle("bt-cabecalho");
+});
+
+navegacao.addEventListener("click", () => {
+    navegacao.classList.toggle("bt-cabecalho");
+})
