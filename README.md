ddded9fa4321379105eff5a965f14c8e32d24f20
Dhayana <contatodhayana@gmail.com>
2020-06-24 18:49:00 -0300

    alteracoes layout

--- README.md
+++ README.md
@@ -27,6 +27,6 @@ Este é um simples projeto com a finalidade de treinar habilidades relativa as l
 ## Executar
 **Para executar este projeto na sua máquina, siga as instruções:**
 * execute o seguinte comando no seu terminal: 
-> $ git clone https://github.com/dhayananascimento/galeria-de-fotos.git
+``` $ git clone https://github.com/dhayananascimento/galeria-de-fotos.git ```
 * abra a pasta que será  criada
 * abra o arquivo "index.html" com o navegador de sua preferência
--- script.js
+++ script.js
@@ -1,219 +1,156 @@
-const imagens = [
+const dados = [
     {
-      src: "https://images.unsplash.com/photo-1587842495357-9d810efc6a69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-      alt: "img",
-      tipo: "comidas"
+      alt: "imagem 01",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
+    },  
+    {
+      alt: "imagem 02",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1517947138120-81858a0b2ab7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=333&q=80",
-      alt: "img",
-      tipo: "comidas"
+      alt: "imagem 03",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=685&q=80",
-      alt: "img",
-      tipo: "comidas"
+      alt: "imagem 04",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1426869981800-95ebf51ce900?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-      alt: "img",
-      tipo: "comidas"
+      alt: "imagem 05",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1568600891621-50f697b9a1c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-      alt: "img",
-      tipo: "comidas"
+      alt: "imagem 06",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1549007953-2f2dc0b24019?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-      alt: "img",
-      tipo: "comidas"
+      alt: "imagem 07",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1582034986517-30d163aa1da9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      alt: "img",
-      tipo: "comidas"
+      alt: "imagem 08",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1496256654245-8f3d0ef3bebe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-      alt: "img",
-      tipo: "flor"
+      alt: "imagem  09",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1496932388010-82366a36677e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-      alt: "img",
-      tipo: "flores"
+      alt: "imagem 10",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1581621222102-2d4e2ae21256?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=625&q=80",
-      alt: "img",
-      tipo: "flores"
+      alt: "imagem 11",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1562628379-791d08579b5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1528&q=80",
-      alt: "img",
-      tipo: "flores"
+      alt: "imagem 12",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1505609843853-f9bc4956d85c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-      alt: "img",
-      tipo: "flores"
+      alt: "imagem 13",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1588656699187-85ea028553d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-      alt: "img",
-      tipo: "flores"
+      alt: "imagem 14",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1582783000910-79f5e993e8a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
-      alt: "img",
-      tipo: "flores"
+      alt: "imagem 15",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1531603071569-0dd65ad72d53?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=716&q=80",
-      alt: "img",
-      tipo: "flores"
+      alt: "imagem 16",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1500479694472-551d1fb6258d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" ,
-      alt: "img",
-      tipo: "animais"
+      alt: "imagem 17",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1470619549108-b85c56fe5be8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=387&q=80" ,
-      alt: "img",
-      tipo: "animais"
+      alt: "imagem 18",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1496316980714-b041aa7fa802?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=332&q=80" ,
-      alt: "img",
-      tipo: "animais"
+      alt: "imagem 19",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" ,
-      alt: "img",
-      tipo: "animais"
+      alt: "imagem 20",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1503256207526-0d5d80fa2f47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=333&q=80" ,
-      alt: "img",
-      tipo: "animais"
+      alt: "imagem 21",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1543757177-b2d52493282e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" ,
-      alt: "img",
-      tipo: "animais"
+      alt: "imagem 22",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1445820200644-69f87d946277?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=333&q=80" ,
-      alt: "img",
-      tipo: "animais"
+      alt: "imagem 23",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
     {
-      src: "https://images.unsplash.com/photo-1570500420685-027d9aa45bb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" ,
-      alt: "img",
-      tipo: "animais"
+      alt: "imagem 24",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
     },
-                    
     {
-      src: "https://images.unsplash.com/photo-1574451987955-6395ba377b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80", 
-      alt: "img",
-      tipo: "paisagens"
-    }, 
-    {
-      src: "https://images.unsplash.com/photo-1518142275918-24377c2e490b?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80", 
-      alt: "img",
-      tipo: "paisagens"
-    }, 
-    {
-      src: "https://images.unsplash.com/photo-1504700610630-ac6aba3536d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
-      alt: "img",
-      tipo: "paisagens"
-    }, 
-    {
-      src: "https://images.unsplash.com/photo-1583214552082-dff0bb815203?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80", 
-      alt: "img",
-      tipo: "paisagens"
-    }, 
-    {
-      src: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=840&q=80", 
-      alt: "img",
-      tipo: "paisagens"
-    }, 
-    {
-      src: "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
-      alt: "img",
-      tipo: "paisagens"
-    }, 
-    {
-      src: "https://images.unsplash.com/photo-1483086431886-3590a88317fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80", 
-      alt: "img",
-      tipo: "paisagens"
-    }, 
-    {
-      src: "https://images.unsplash.com/photo-1431794062232-2a99a5431c6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
-      alt: "img",
-      tipo: "paisagens"
-    },    
-];
+      alt: "imagem 25",
+      src: "imagem.jpg",
+      autor: "Photo by matthew reyes on Unsplash"
+    }
+]
 
-const bt_navegacao = document.getElementById("bt-navegacao");
-const navegacao = document.getElementById("navegacao");
-const bt_topo = document.getElementById("bt-topo");
 
-const cabecalho = () => {
-  bt_navegacao.addEventListener("click", () => navegacao.classList.toggle("bt-cabecalho"));
-  navegacao.addEventListener("click", () => {
-    if(window.innerWidth <= 768)
-      navegacao.classList.toggle("bt-cabecalho");
-  });
+const botaoTopo = document.getElementById("botaoTopo")
+const containerGaleria = document.getElementById("containerGaleria")
+
+function galeria() {
+    dados.forEach((element) => {
+      let container = document.createElement("div")
+      container.classList.add("containerImagem")
+
+      let imagem = document.createElement("img")
+      imagem.setAttribute("src", `imagens/${element.src}`)
+      imagem.setAttribute("alt", element.alt)
+
+      let titulo = document.createElement("h3")
+      titulo.appendChild(document.createTextNode(element.alt))
+
+      let autor = document.createElement("p")
+      autor.appendChild(document.createTextNode(element.autor))
+
+      container.appendChild(imagem)
+      container.appendChild(titulo)
+      container.appendChild(autor)
+
+      containerGaleria.appendChild(container)
+    })
 }
 
-const carregaSecao = (id) => {
-    let secao = document.getElementById(id);
-
-    let container = document.createElement("div");
-    container.classList.add("secao");
-
-    let titulo = document.createElement("h2");
-    titulo.appendChild(document.createTextNode(id));
-
-    imagens.forEach((element) => {
-        if(element.tipo == id) {
-            let containerImagem = document.createElement("div");
-            containerImagem.classList.add("container-imagem");
-
-            let imagem = document.createElement("img");
-            imagem.setAttribute("src", element.src);
-            imagem.setAttribute("alt", element.alt);
-
-            containerImagem.appendChild(imagem);
-            container.appendChild(containerImagem);
-        }
-    });
-
-    secao.appendChild(titulo);
-    secao.appendChild(container);
-}
-
-const secoes = () => {
-    let secao = document.querySelectorAll("section");
-    secao.forEach(element =>  carregaSecao(element.id));
-}
-
-const topo = () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-const rolamento = () => {
-  if(document.body.scrollTop > 60 || document.documentElement.scrollTop > 60)
-    bt_topo.style.display = "block";
-  else
-    bt_topo.style.display="none";
-    
-}
-
-window.onscroll = () => rolamento();
-cabecalho();
-secoes();
+galeria()
